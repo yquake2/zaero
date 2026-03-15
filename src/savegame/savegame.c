dc05bc6d7119182955d64c71c0744b870058b346
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-15 15:25:27 +0000

    savegame: Prevent indexing edicts / clients / items out of bounds
    The code used the index value written in the save file without bounds checking, so an item index of -2 or 334 for example would lead to illegal memory access or undefined behavior

--- src/savegame/savegame.c
+++ src/savegame/savegame.c
@@ -613,7 +613,7 @@ ReadField(FILE *f, field_t *field, byte *base)
 		case F_EDICT:
 			index = *(int *)p;
 
-			if (index == -1)
+			if ((index < 0) || (index >= game.maxentities))
 			{
 				*(edict_t **)p = NULL;
 			}
@@ -626,7 +626,7 @@ ReadField(FILE *f, field_t *field, byte *base)
 		case F_CLIENT:
 			index = *(int *)p;
 
-			if (index == -1)
+			if ((index < 0) || (index >= game.maxclients))
 			{
 				*(gclient_t **)p = NULL;
 			}
@@ -639,7 +639,7 @@ ReadField(FILE *f, field_t *field, byte *base)
 		case F_ITEM:
 			index = *(int *)p;
 
-			if (index == -1)
+			if ((index < 0) || (index >= game.num_items))
 			{
 				*(gitem_t **)p = NULL;
 			}
