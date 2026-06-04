4438be084d19e9db3a354525f399ed4e5a33e8e7
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-06-04 08:21:33 +0000

    game: Fixed Z_RadiusDamage returning early if ignore is NULL

--- src/zaero/weapon.c
+++ src/zaero/weapon.c
@@ -1329,7 +1329,7 @@ void Z_RadiusDamageVisible(edict_t *inflictor, edict_t *attacker, float damage, 
 	vec3_t	v;
 	vec3_t	dir;
 
-	if (!inflictor || !attacker || !ignore)
+	if (!inflictor || !attacker)
 	{
 		return;
 	}
